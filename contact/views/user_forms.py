4b37fb4b217b557bd3cc19dc4fe2df02f3553d71
Davi Ruas <daviruasfb@gmail.com>
2025-04-01 20:26:15 -0300

    User auth fuctions

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -3,6 +3,7 @@ from contact.forms import RegisterForm, RegisterUpdateForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
 from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 
 
@@ -49,16 +50,18 @@ def login_view(request):
         
     return render(
         request,
-        'contact/register.html',
+        'contact/login.html',
         context
         
     )
-    
+ 
+@login_required(login_url='contact:login')    
 def logout_view(request):
     auth.logout(request)
     
     return redirect('contact:login')
 
+@login_required(login_url='contact:login')
 def user_update(request):
     form = RegisterUpdateForm(instance=request.user)
         
